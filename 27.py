fa838b11cd3dff5873573bdeb44fdf4403a92a11
bresha <hrvoje.bresic@gmail.com>
2021-11-12 19:23:47 +0100

    refactored 27.py

--- 27.py
+++ 27.py
@@ -19,12 +19,7 @@
 '''
 
 import os
-cur_dir = os.getcwd()
-files_dir = 'horoskop'
-files_dir = os.path.join(cur_dir, files_dir)
-files_list = os.listdir(files_dir)
-
-list_of_peoples = []
+import csv
 
 def check_for_name_and_years(line):
     x = line.split()
@@ -35,8 +30,6 @@ def check_for_name_and_years(line):
                 contains_number = True
 
     return contains_number
-
-
 
 def check_for_sign(line):
     list_of_signs = ["bik", "blizanac", "djevica", "jarac", "lav", "ovan", "rak", "riba", "škorpion", "strijelac", "vaga", "vodenjak"]
@@ -56,49 +49,79 @@ def check_for_tel(line):
         return True
     return False
  
-for file in files_list:
-    dat = {}
-    file_path = os.path.join(files_dir, file)
-    with open(file_path, encoding="utf8") as f:
-        lines = f.readlines()
-        for line in lines:
-            if check_for_name_and_years(line):
-                x = line.split(' ')
-                name = x[0].strip(',')
-                years = x[1].strip()
-                dat["name"] = name
-                dat["years"] = years
-            elif check_for_sign(line):
-                dat["sign"] = line.strip().lower()
-            elif check_for_email(line):
-                dat["email"] = line.strip()
-            elif check_for_tel(line):
-                dat["tel"] = line.strip()
-            else:
-                if "desc" in dat.keys():
-                    dat["desc"].append(line.strip())
+def read_files():
+    cur_dir = os.getcwd()
+    files_dir = 'horoskop'
+    files_dir = os.path.join(cur_dir, files_dir)
+    files_list = os.listdir(files_dir)
+
+    list_of_peoples = []
+
+    for file in files_list:
+        dat = {}
+        file_path = os.path.join(files_dir, file)
+        with open(file_path, encoding="utf8") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                if check_for_name_and_years(line):
+                    x = line.split(' ')
+                    dat["name"] = x[0].strip(',')
+                    dat["years"] = x[1]
+                elif check_for_sign(line):
+                    dat["sign"] = line.lower()
+                elif check_for_email(line):
+                    dat["email"] = line
+                elif check_for_tel(line):
+                    dat["tel"] = line
                 else:
-                    dat["desc"] = [line.strip()]
-    list_of_peoples.append(dat)
+                    if "desc" in dat.keys():
+                        dat["desc"] = dat["desc"] + ' ' + line
+                    else:
+                        dat["desc"] = line
 
-files = list_of_peoples[0].keys()
+        list_of_peoples.append(dat)
 
-# create new dir
-newpath = os.path.join(cur_dir, 'new')
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+    return list_of_peoples
 
-for file in files:
-    data = []
-    for people in list_of_peoples:
-        data.append(people[file])
+def write_files(list_of_peoples):
+    files = list_of_peoples[0].keys()
 
-    file_path = os.path.join(newpath, f"{file}.txt")
-    with open(file_path, 'w+', encoding="utf8") as f:
-        for value in data:
-            if file != "desc":
+    cur_dir = os.getcwd()
+    newpath = os.path.join(cur_dir, 'new')
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    for file in files:
+        data = []
+        for people in list_of_peoples:
+            data.append(people[file])
+
+        file_path = os.path.join(newpath, f"{file}.txt")
+        with open(file_path, 'w+', encoding="utf8") as f:
+            for value in data:
                 f.write(f"{value}\n")
-            else:
-                for el in value:
-                    f.write(f"{el}\n")
 
+def write_csv(list_of_peoples):
+    cols = list_of_peoples[0].keys()
+
+    cur_dir = os.getcwd()
+    newpath = os.path.join(cur_dir, 'new')
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    file_path = os.path.join(newpath, "Tinder.csv")
+    with open(file_path, 'w+', newline='', encoding="utf8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=cols)
+        writer.writeheader()
+        for people in list_of_peoples:
+            writer.writerow(people)
+            print(people)        
+
+def main():
+    list_of_peoples = read_files()
+    write_files(list_of_peoples)
+    write_csv(list_of_peoples)
+
+if __name__ == "__main__":
+    main()
